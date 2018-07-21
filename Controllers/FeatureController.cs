66f6bc7b13fe55a31b2964e70a0c417702b36610
Andy Ba <xbax486@gmail.com>
2018-07-21 14:12:12 +1000

    Implement the API to retrieve features

---
+++ Controllers/FeatureController.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using vega.Controllers.Resources;
+using vega.Models;
+using vega.Persistence;
+
+namespace vega.Controllers
+{
+    public class FeatureController
+    {
+        private readonly VegaDbContext context;
+        private readonly IMapper mapper;
+        public FeatureController(VegaDbContext context, IMapper mapper)
+        {
+            this.mapper = mapper;
+            this.context = context;
+        }
+
+        [HttpGet("/api/features")]
+        public async Task<IEnumerable<FeatureResource>> GetFeatures()
+        {
+            var features = await context.Features.ToListAsync();
+            return mapper.Map<List<Feature>, List<FeatureResource>>(features);
+        }
+    }
+}
