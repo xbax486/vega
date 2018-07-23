f1cbf90248e65e1cffed46449cc1a519712a33a9
Andy Ba <xbax486@gmail.com>
2018-07-23 15:40:29 +1000

    Add Vehicle and VehicleFeature classes

--- Migrations/20180723052830_AddVehicle.Designer.cs
+++ Migrations/20180723052830_AddVehicle.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using vega.Persistence;
 
 namespace vega.Migrations
 {
     [DbContext(typeof(VegaDbContext))]
-    partial class VegaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180723052830_AddVehicle")]
+    partial class AddVehicle
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Models/Vehicle.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace vega.Models
+{
+    [Table("Vehicles")]
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public int ModelId { get; set; }
+
+        public Model Model { get; set; }
+
+        public bool IsRegistered { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string ContactName { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string ContactPhone { get; set; }
+
+        [StringLength(255)]
+        public string ContactEmail { get; set; }
+
+        public DateTime LastUpdated { get; set; }
+
+        public ICollection<VehicleFeature>  VehicleFeatures { get; set; }
+
+        public Vehicle()
+        {
+            VehicleFeatures = new Collection<VehicleFeature>();
+        }
+    }
+}
