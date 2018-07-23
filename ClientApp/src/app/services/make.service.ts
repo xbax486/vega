0b8886817d1564002d04fe1c39cd0de76a2deb35
Andy Ba <xbax486@gmail.com>
2018-07-23 10:51:19 +1000

    Use HttpClient for http services

--- ClientApp/src/app/services/make.service.ts
+++ ClientApp/src/app/services/make.service.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
 @Injectable()
 export class MakeService {
 
-  constructor(private http: Http) { }
+  constructor(private http: HttpClient) { }
 
   getMakes(){
     return this.http.get('/api/makes').pipe(
-      map(res => res.json())
+      map(res => res)
     );
   }
 }
