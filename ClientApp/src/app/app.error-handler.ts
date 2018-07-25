858b61f86bda8b7abf4782b3a9723666538e41a0
Andy Ba <xbax486@gmail.com>
2018-07-25 21:19:15 +1000

    Use NgZone to add toastr event into Angular zone

--- ClientApp/src/app/app.error-handler.ts
+++ ClientApp/src/app/app.error-handler.ts
@@ -1,20 +1,22 @@
-import { ErrorHandler, Inject, Injector } from "@angular/core";
+import { ErrorHandler, Inject, Injector, NgZone, isDevMode } from "@angular/core";
 import { ToastrService } from 'ngx-toastr';
 
 export class AppErrorHandler implements ErrorHandler {
-    constructor(@Inject(Injector) private injector: Injector) {}
+    constructor(@Inject(Injector) private injector: Injector, @Inject(NgZone) private ngZone: NgZone) {}
 
     // Need to get ToastrService from injector rather than constructor injection to avoid cyclic dependency error
     private get toastrService(): ToastrService {
         return this.injector.get(ToastrService);
     }
-
+    
     handleError(error: any): void {
-        console.log("ERROR");
-        this.toastrService.error(error.statusText, 'Error', {
-            closeButton: true,
-            timeOut: 5000,
-            onActivateTick: true
+        if(isDevMode)
+            throw error;
+        this.ngZone.run(() => {
+            this.toastrService.error(error.statusText, 'Error', {
+                closeButton: true,
+                timeOut: 5000
+            });
         });
     }
 }
